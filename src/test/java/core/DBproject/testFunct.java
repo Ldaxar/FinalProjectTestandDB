56236db653330d6fd9a1353fe3721f80ef709014
teutzue_1995@yahoo.com <teutzue_1995@yahoo.com>
2017-05-08 15:04:41 +0200

    sasasaaA

--- src/test/java/core/DBproject/testFunct.java
+++ src/test/java/core/DBproject/testFunct.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 /**
  * Created by CosticaTeodor on 08/05/2017.
  */
-public class testFunctIT {
+public class testFunct {
 
     @Test
     public void testFunctionality() {
